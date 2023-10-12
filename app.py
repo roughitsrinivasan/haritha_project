dab3b7fc37aa966ad5d7b96103600368f17f548f
Sudhir <sudirsha02@gmail.com>
2023-10-13 00:34:47 +0530

    update

---
+++ app.py
@@ -0,0 +1,148 @@
+from flask import Flask, render_template, request , redirect,url_for, session
+from flask_session import Session
+
+from utils import *
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+def start():
+    return redirect(url_for('login'))
+    return render_template('start.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        pwd = request.form['pwd']
+        print(user)
+        print(pwd)
+
+        
+        if user=="haritha" and pwd == "haritha@123":
+            session["name"] = user
+            
+            return redirect(url_for('home'))
+        elif user=="admin" and pwd == "admin":
+            session["name"] = "admin"
+            return redirect(url_for('home'))
+        else:
+            return render_template('not.html')
+    session['purchased']=[]
+    session['cart']={}
+            
+    return render_template('login.html')
+    # else:
+    #     user = request.args.get('nm')
+    #     return redirect(url_for('home', name=user))
+
+@app.route('/home')
+def home():
+    if not session.get("name"):
+        return "Not a valid user"
+    return render_template('home.html')
+
+@app.route('/details1')
+def details1():
+    if not session.get("name"):
+        return "Not a valid user"
+
+    return render_template('details1.html')
+
+@app.route('/details2')
+def details2():
+    if not session.get("name"):
+        return "Not a valid user"
+
+    return render_template('details2.html')
+
+@app.route('/details3')
+def details3():
+    if not session.get("name"):
+        return "Not a valid user"
+
+    return render_template('details3.html')
+
+@app.route('/details4')
+def details4():
+    if not session.get("name"):
+        return "Not a valid user"
+
+    return render_template('details4.html')
+
+
+@app.route('/cart',methods=['POST','GET'])
+def cart():
+    if not session.get("name"):
+        return "Not a valid user"
+    if request.method=='post':
+        count= request.form.get('count')
+        price=request.form.get('prod-price')
+        name=request.form.get('prod-name')
+        print(request.form.to_dict())
+        total_price=int(count)*int(price)      
+        session['cart']={"name":name,"count":count,"price":price,"total_price":total_price}
+        print(session['cart'])
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    if not session.get("name"):
+        return "Not a valid user"
+    print(session.get('cart'))
+    if session.get('purchased',None)==None:
+        session['purchased']=[]
+    session["purchased"].append(session['cart'])
+    return render_template('cash.html')
+
+@app.route('/order',methods=['POST','GET'])
+def order():
+    if not session.get("name"):
+        return "Not a valid user"
+    if request.method=='POST':
+        name=request.form.get('con_name')
+        email=request.form.get('con_email')
+        phone=request.form.get('con_phone')
+        address=request.form.get('con_address') 
+        import random
+        item=[{
+                "name":"Printed round neck T-shirt",
+                "price":"299.99",
+                "quantity":random.randint(1,10),
+                "total":random.randint(1,10)*299.99
+            },
+            {
+                "name":"Blue White crop top",
+                "price":"599.99",
+                "quantity":random.randint(1,10),
+                "total":random.randint(1,10)*599.99
+            }]
+
+        insert_order(name,email,phone,address,item)
+        return redirect(url_for('order'))
+    return render_template('thankyou.html')
+
+@app.route('/orders')
+def orders():
+    if not session.get("name"):
+        return "Not a valid user"
+    print(session.get('name'))
+    if session.get('name')=="admin":
+        print("admin")
+        # products    =   load_orders_from_json()
+        products    =   load_orders_from_json()
+    # print(products,session.get("name")) 
+        return render_template('orders.html',orders=products)
+    else:
+        return redirect(url_for('login'))
+    
+@app.route('/logout',methods=['POST','GET'])
+def logout():
+    if not session.get("name"):
+        return "Not a valid user"
+    session.pop('name',None)
+    return redirect(url_for('login'))
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ utils.py
@@ -0,0 +1,62 @@
+import json
+
+# Create an empty list to store orders
+orders = []
+
+# Function to save the orders to a JSON file
+def save_orders_to_json():
+    with open("orders.json", "w") as json_file:
+        json.dump(orders, json_file)
+
+# Function to load orders from the JSON file
+def load_orders_from_json():
+    try:
+        with open("orders.json", "r") as json_file:
+            orders = json.load(json_file)
+            return orders
+    except FileNotFoundError:
+        # If the file doesn't exist, create an empty JSON file
+        save_orders_to_json()
+
+# Function to insert a new order
+def insert_order(name, email, phone, address, items):
+    order = {
+        "user_id": len(orders) + 1,
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "address": address,
+        "products": items
+    }
+    orders.append(order)
+    save_orders_to_json()
+
+# Function to fetch an order by user_id
+def fetch_order_by_user_name(user_name):
+    for order in orders:
+        if order["name"] == user_name:
+            return order
+    return None
+
+
+
+# if __name__ == "__main__":
+#     # Load existing orders from the JSON file
+#     load_orders_from_json()
+
+#     # Insert a sample order
+#     insert_order("John Doe", "john@example.com", "123-456-7890", "123 Main St, City", "Product A, Product B, Product C")
+
+#     # Fetch an order by user_id
+#     user_id_to_fetch = 1  # Replace with the user_id you want to fetch
+#     order = fetch_order_by_user_name(user_id_to_fetch)
+
+#     if order:
+#         print(f"User ID: {order['user_id']}")
+#         print(f"Name: {order['name']}")
+#         print(f"Email: {order['email']}")
+#         print(f"Phone: {order['phone']}")
+#         print(f"Address: {order['address']}")
+#         print(f"Items: {order['items']}")
+#     else:
+#         print(f"No order found for user ID {user_id_to_fetch}")
